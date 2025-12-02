b9ba72a4294e60912d071167bddc5b70e8132ee9
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-12-02 20:21:08 +0500

    Background Color Changer Created

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
 
   return (
     <>
+      <div className='flex justify-center' style={{backgroundColor: color}}>
+        <button className='px-3 py-1 bg-[red] m-2 rounded cursor-pointer'
+        onClick={()=>setColor("red")}
+        >Red</button>
+        <button className='px-3 py-1 bg-[green] m-2 rounded cursor-pointer'
+        onClick={()=>setColor("green")}
+        >Green</button>
+        <button className='px-3 py-1 bg-[blue] m-2 rounded cursor-pointer'
+        onClick={()=>setColor("blue")}
+        >Blue</button>
+      </div>
 
+      <div></div>
     </>
   )
 }
