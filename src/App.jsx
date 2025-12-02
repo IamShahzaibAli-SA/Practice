b17d0bdee4ad1a2b8ab0cb28c842bf6fc03a1799
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-12-02 20:14:27 +0500

    First Commit of the Day

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("black");
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
